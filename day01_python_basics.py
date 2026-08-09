ccb827367947ced37371e9e6eed2d72ff19dec97
adtrj-tech <adithraj2015@gmail.com>
2026-08-09 11:56:51 +0530

    Add files via upload

---
+++ day01_python_basics.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+
+name= "Adith Raj"
+course= "Data Science and AI"
+
+print(f"Name: {name}")
+print(f"Course: {course}")
---
+++ day02_python_intro.py
@@ -0,0 +1,25 @@
+# Day 2 - Introduction to Python
+
+print("Hello, Python!")
+
+# Variables
+name = "Adith Raj"
+age = 22
+field = "AI & Data Science"
+
+print("Name:", name)
+print("Age:", age)
+print("Field:", field)
+
+# Basic Data Types
+name_type = type(name)
+age_type = type(age)
+
+print("Type of name:", name_type)
+print("Type of age:", age_type)
+
+# Basic arithmetic
+a = 10
+b = 20
+
+print("Sum:", a + b)
